8bd8df7c02977f8780add989dedd520dcbcc678a
imygnam <imygnam@gmail.com>
2022-07-03 17:22:11 +0900

    [FEAT] initial commit

---
+++ src/main/java/com/lolduo/duo/DuoApplication.java
@@ -0,0 +1,13 @@
+package com.lolduo.duo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lolduo/duo/DuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolduo.duo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
