c01f930bb665cc2c01df77af10656183e73e8a6a
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-05 02:34:42 +0900

    [Fix] json column

--- src/main/java/com/lolduo/duo/entity/gameInfo/TrioEntity.java
+++ src/main/java/com/lolduo/duo/entity/gameInfo/TrioEntity.java
@@ -30,7 +30,7 @@ public class TrioEntity {
 
 
     @Type(type = "json")
-    @Column(name = "position")
+    @Column(name = "position",columnDefinition = "json")
     private Map<Long,String> positionMap;
 
     @Type(type = "json")
@@ -42,7 +42,7 @@ public class TrioEntity {
     private Map<Long,TreeSet<Long>> spellListMap;
 
     @Type(type = "json")
-    @Column(name = "champion")
+    @Column(name = "champion",columnDefinition = "json")
     private TreeSet<Long> championList;
 
     @Type(type = "json")
