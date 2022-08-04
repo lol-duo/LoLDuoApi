f10b51568f7eeb98f3eb27adc79f82394d73c7e6
Inha Yoon <survedog@gmail.com>
2022-08-04 23:35:10 +0900

    [FEAT] 듀오 검색 추가 (ALL 미구현)

--- src/main/java/com/lolduo/duo/dto/client/ChampionInfoListDTO.java
+++ src/main/java/com/lolduo/duo/dto/client/ChampionInfoListDTO.java
@@ -1,6 +1,5 @@
 package com.lolduo.duo.dto.client;
 
-import com.lolduo.duo.entity.ChampionEntity;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,19 +7,19 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 @Getter
 @NoArgsConstructor
-public class ChampionInfoDTOList implements Comparable<ChampionInfoDTOList>{
+public class ChampionInfoListDTO implements Comparable<ChampionInfoListDTO>{
     private List<ClinetChampionInfoDTO> clinetChampionInfoDTOList;
     @ApiModelProperty(example = "58.7%")
     private String winRate;
 
-    public ChampionInfoDTOList(List<ClinetChampionInfoDTO> clinetChampionInfoDTOList, String winRate) {
+    public ChampionInfoListDTO(List<ClinetChampionInfoDTO> clinetChampionInfoDTOList, String winRate) {
         this.clinetChampionInfoDTOList = clinetChampionInfoDTOList;
         this.winRate = winRate;
     }
 
 
     @Override
-    public int compareTo(ChampionInfoDTOList championInfoDTOList) {
+    public int compareTo(ChampionInfoListDTO championInfoDTOList) {
         int a = Double.valueOf(this.winRate.substring(0,this.winRate.length()-1)).intValue();
         int b= Double.valueOf(championInfoDTOList.getWinRate().substring(0,championInfoDTOList.getWinRate().length()-1)).intValue();
         if(b < a)
--- src/main/java/com/lolduo/duo/repository/gameInfo/DuoRepository.java
+++ src/main/java/com/lolduo/duo/repository/gameInfo/DuoRepository.java
@@ -4,12 +4,9 @@ import com.lolduo.duo.entity.gameInfo.DuoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.TreeSet;
 
 public interface DuoRepository extends JpaRepository<DuoEntity, Long> {
     @Query(value = "select * from duo where json_contains(champion,?1) and  json_contains(position,?2)",nativeQuery = true)
-    Optional<DuoEntity> findByChampionandpAndPosition(String championId, String position);
+    Optional<DuoEntity> findByChampionAndPosition(String championId, String position);
 }
