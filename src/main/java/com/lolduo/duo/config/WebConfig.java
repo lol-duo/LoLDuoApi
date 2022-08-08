fa1f5441e34e876ea4b8ec2c44f84262d9ec4595
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-09 02:12:17 +0900

    [Fix] log added

---
+++ src/main/java/com/lolduo/duo/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.lolduo.duo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://lolduo.net")
+                .allowedOrigins("https://lolduo.net");
+    }
+}
--- src/main/java/com/lolduo/duo/controller/ClientApi.java
+++ src/main/java/com/lolduo/duo/controller/ClientApi.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-@CrossOrigin(originPatterns = "*")
+
 @RestController
 @RequiredArgsConstructor
 public class ClientApi {
