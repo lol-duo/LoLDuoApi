5bf08beda88e661d54ece2de46fcb5a4e4401a36
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-09 15:58:01 +0900

    [Fix] indivisual position-> team postion

--- src/main/java/com/lolduo/duo/repository/gameInfo/MatchIdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lolduo.duo.repository.gameInfo;
-
-import com.lolduo.duo.entity.gameInfo.MatchIdEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchIdRepository extends JpaRepository<MatchIdEntity,Long> {
-}
