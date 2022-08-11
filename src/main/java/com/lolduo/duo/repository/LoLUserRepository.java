fb70d8a1307e6784a548dfd1d000486724c905a5
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-11 09:33:34 +0900

    [Feature] DAM-79 : 기존 매치 승률 정보줄 때 총 판수도 던져주기

--- src/main/java/com/lolduo/duo/repository/LoLUserRepository.java
+++ src/main/java/com/lolduo/duo/repository/LoLUserRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface LoLUserRepository extends JpaRepository<LoLUserEntity, String> {
-    @Query(value = "select LoLUserEntity.puuid from LoLUserEntity where LoLUserEntity.tier = ?1")
+    //@Query(value = "select LoLUserEntity.puuid from LoLUserEntity where LoLUserEntity.tier = ?1")
+    //List<String> findPuuidsByLeague (String league);
+
+    @Query(value = "select puuid from lol_user where tier = ?1",nativeQuery = true)
     List<String> findPuuidsByLeague (String league);
 }
