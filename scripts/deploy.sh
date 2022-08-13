1500bcbdf10094088ce48e87688c9c268feda2c9
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-13 23:32:22 +0900

    [Feature] devServer 추가 위해 deploy.sh, application.yml 파일 수

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 REPOSITORY=/home/build
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+DATADOG=$(ls -tr $REPOSITORY/datadog/*.jar | tail -n 1)
+CURRENT_PID=$(pgrep -fla $JAR_NAME | awk '{print $1}')
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fla /home/build/duo-0.0.1-SNAPSHOT.jar | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -12,8 +14,8 @@ else
   sleep 5
 fi
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-DATADOG=$(ls -tr $REPOSITORY/datadog/*.jar | tail -n 1)
+
+
 echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
--- scripts/devScripts/deploy.sh
+++ scripts/devScripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-REPOSITORY=/home/build
-DEVREPOSITORY=/home/build/dev
+REPOSITORY=/home/build/dev
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+DATADOG=$(ls -tr $REPOSITORY/datadog/*.jar | tail -n 1)
+CURRENT_PID=$(pgrep -fla $JAR_NAME | awk '{print $1}')
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fla /home/build/dev/duo-0.0.1-SNAPSHOT.jar | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -13,11 +14,11 @@ else
   sleep 5
 fi
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr DEVREPOSITORY/*.jar | tail -n 1)
-DATADOG=$(ls -tr $REPOSITORY/datadog/*.jar | tail -n 1)
+
+
 echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -javaagent:$DATADOG -Ddd.logs.injection=true -Ddd.service=api-dev-server -jar -Dspring.profiles.active=devserver $JAR_NAME > DEVREPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -javaagent:$DATADOG -Ddd.logs.injection=true -Ddd.service=api-dev-server -jar -Dspring.profiles.active=devserver $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
