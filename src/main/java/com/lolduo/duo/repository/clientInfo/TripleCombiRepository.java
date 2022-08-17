fadaf852eb6b8a712359930546b6e16a1099df96
Inha Yoon <survedog@gmail.com>
2022-08-17 21:15:19 +0900

    [FIX] TripleCombiRepository에서 triple_info -> triple_combi로 오타 수정

--- src/main/java/com/lolduo/duo/repository/clientInfo/TripleCombiRepository.java
+++ src/main/java/com/lolduo/duo/repository/clientInfo/TripleCombiRepository.java
@@ -8,18 +8,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TripleCombiRepository extends JpaRepository<TripleCombiEntity,Long>, ICombiRepository {
-    @Query(value = "select * from triple_info where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by win_count / all_count DESC limit 30",nativeQuery = true)
+    @Query(value = "select * from triple_combi where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by win_count / all_count DESC limit 30",nativeQuery = true)
     List<TripleCombiEntity> findAllByChampionIdAndPositionWinRateDesc(String championId, String position, String positionList, String excludePositionList);
 
-    @Query(value = "select * from triple_info where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by win_count / all_count ASC limit 30",nativeQuery = true)
+    @Query(value = "select * from triple_combi where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by win_count / all_count ASC limit 30",nativeQuery = true)
     List<TripleCombiEntity> findAllByChampionIdAndPositionWinRateAsc(String championId, String position, String positionList, String excludePositionList);
 
-    @Query(value = "select * from triple_info where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by all_count DESC limit 30",nativeQuery = true)
+    @Query(value = "select * from triple_combi where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by all_count DESC limit 30",nativeQuery = true)
     List<TripleCombiEntity> findAllByChampionIdAndPositionGameCountDesc(String championId, String position, String positionList, String excludePositionList);
 
-    @Query(value = "select * from triple_info where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by all_count ASC limit 30",nativeQuery = true)
+    @Query(value = "select * from triple_combi where all_count >= 10 and json_contains(champion_id,?1) and json_contains(position,?2) and json_contains(json_extract(position, '$.*'), ?3) and not json_has_exclude_position(position, ?1, ?4) order by all_count ASC limit 30",nativeQuery = true)
     List<TripleCombiEntity> findAllByChampionIdAndPositionGameCountAsc(String championId, String position, String positionList, String excludePositionList);
 
-    @Query(value = "select * from triple_info where json_contains(champion_id,?1) and json_contains(position,?2) limit 1",nativeQuery = true)
+    @Query(value = "select * from triple_combi where json_contains(champion_id,?1) and json_contains(position,?2) limit 1",nativeQuery = true)
     Optional<TripleCombiEntity> findByChampionIdAndPosition(String championId, String position);
 }
