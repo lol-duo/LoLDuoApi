29a0af9c7b12b6adc5dcaf7344f7bfc206518aeb
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-08-30 10:27:54 +0900

    [FIX] combiEntity가 null일때 판수조건 5판에서 3판 한번더 search해보도록 완화

--- src/main/java/com/lolduo/duo/service/ClientService.java
+++ src/main/java/com/lolduo/duo/service/ClientService.java
@@ -152,8 +152,16 @@ public class ClientService {
             return new ResponseEntity<>("404 BAD_REQUEST", HttpStatus.OK);
         }
         if(combiEntity==null){
-            log.info("getChampionDetail2 - 챔피언은 존재하나, 신화를 산 내역이 존재하지 않습니다. Entitiy가 NULL입니다. ");
-            return new ResponseEntity<>("챔피언은 찾았으나 신화를 산 기록이 존재하지 않습니다. Entitiy가 NULL입니다.", HttpStatus.OK);
+            try {
+                combiEntity = combiRepository.findByPerkAndMythItemAndPositionAndWinRateDesc(objectMapper.writeValueAsString(championPositionMap), 3L).orElse(null);
+            } catch (JsonProcessingException e) {
+                log.error("getChampionDetail2 - objectMapper writeValue error");
+                return new ResponseEntity<>("404 BAD_REQUEST", HttpStatus.OK);
+            }
+            if(combiEntity==null) {
+                log.info("getChampionDetail2 - 챔피언은 존재하나, 신화를 산 내역이 존재하지 않습니다. Entitiy가 NULL입니다. ");
+                return new ResponseEntity<>("챔피언은 찾았으나 신화를 산 기록이 존재하지 않습니다. Entitiy가 NULL입니다.", HttpStatus.OK);
+            }
         }
         result = makeChampionDetail2(winRate,AllCount,combiEntity);
         return new ResponseEntity<>(result,HttpStatus.OK);
