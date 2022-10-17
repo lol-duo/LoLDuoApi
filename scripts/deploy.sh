279598ba4a0ba90af9a0e2a0dc66ec20c870abd6
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-10-17 15:31:49 +0900

    API_v2_DummyData deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@
 
 REPOSITORY=/home/build
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-DATADOG=$(ls -tr $REPOSITORY/datadog/*.jar | tail -n 1)
 CURRENT_PID=$(pgrep -fla $JAR_NAME | awk '{print $1}')
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
@@ -21,4 +20,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -javaagent:$DATADOG -Ddd.logs.injection=true -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
