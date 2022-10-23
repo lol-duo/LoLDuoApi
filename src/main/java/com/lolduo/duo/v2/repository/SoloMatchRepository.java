6da340ce8cf867340710ced5ba12b9e0afae2f78
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-10-23 14:36:18 +0900

    [FEAT][API_V2] 개수 100 -> 30개로 임시 변경

--- src/main/java/com/lolduo/duo/v2/repository/SoloMatchRepository.java
+++ src/main/java/com/lolduo/duo/v2/repository/SoloMatchRepository.java
@@ -11,6 +11,6 @@ public interface SoloMatchRepository extends JpaRepository<SoloMatchEntity, Long
 
     @Query(value = "select floor(sum(all_count)/10) from solo_match",nativeQuery = true)
     Optional<Long> getAllCountSum();
-    @Query(value = "select * from solo_match where all_count > ?3 and  position like ?1 and champion_id like ?2 order by win_count / all_count DESC limit 100",nativeQuery = true)
+    @Query(value = "select * from solo_match where all_count > ?3 and  position like ?1 and champion_id like ?2 order by win_count / all_count DESC limit 30",nativeQuery = true)
     List<SoloMatchEntity> findAllByPositionAndChampionId(String position, String championId,Long allCount);
 }
