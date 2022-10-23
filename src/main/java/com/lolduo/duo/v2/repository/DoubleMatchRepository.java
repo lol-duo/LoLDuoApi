b1570be33f4f5c0b40cc6489ee405f9323e0abee
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-10-23 14:01:17 +0900

    [FEAT][API_V2] 픽률 0.5 % 이상의 데이터에만 출력하는 조건 적용

--- src/main/java/com/lolduo/duo/v2/repository/DoubleMatchRepository.java
+++ src/main/java/com/lolduo/duo/v2/repository/DoubleMatchRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DoubleMatchRepository extends JpaRepository<DoubleMatchEntity, Long> {
-    @Query(value = "select floor(sum(all_count)/10) from double_match",nativeQuery = true)
+    @Query(value = "select floor(sum(all_count)/20) from double_match",nativeQuery = true)
     Optional<Long> getAllCountSum();
 
     @Query(value = "select * from double_match where all_count > ?5 and  position1 like ?1 and champion_id1 like ?2 and position2 like ?3 and champion_id2 like ?4 order by win_count / all_count DESC limit 100",nativeQuery = true)
