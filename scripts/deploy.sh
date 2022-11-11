4a994d55d8c79a96c3efcc727fbf9e9539471ebd
JW-Ahn0 <jun.woo.an0@gmail.com>
2022-11-11 21:55:44 +0900

    [FEAT][API_V2] pinpoint.agentId 이슈, deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 REPOSITORY=/home/build
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 CURRENT_PID=$(pgrep -fla $JAR_NAME | awk '{print $1}')
+IP=$(hostname -I)
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -20,4 +21,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup /opt/jdk-17/bin/java -jar -Dspring.profiles.active=server $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -jar -javaagent:pinpoint-agent-2.2.2/pinpoint-bootstrap-2.2.2.jar -Dpinpoint.agentId=$IP -Dpinpoint.applicationName=API-SERVER -Dpinpoint.config=pinpoint-agent-2.2.2/pinpoint-root.config -Dspring.profiles.active=server $JAR_NAME > /home/build/nohup.out 2>&1 &
